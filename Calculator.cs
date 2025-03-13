67360ad4e3884c8c7b5c3c63a1c737b16e19c250
Raihan <raihansuyuti7@students.amikom.ac.id>
2025-03-13 10:00:41 +0700

    Perubahan 1

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+class Calculator
+{
+	static void Main(string[]args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.Writeline
+	}
+	
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
